d594b1ce516fefa14a5b7d206082505241dd9462
PriyankaBiswal1998 <71176385+PriyankaBiswal1998@users.noreply.github.com>
2020-09-12 17:33:05 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,18 @@
+package Brickbreaker2;
+
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 JFrame obj = new JFrame();
+		obj.setBounds(10, 10, 700, 600);
+		obj.setTitle("Breakout Ball");
+		obj.setResizable(false); 
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE );
+
+	}
+
+}
